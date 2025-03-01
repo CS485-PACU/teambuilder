a0aacf23294799ee9bac2fe1a5cff7c4d96931ec
chadd williams <will4614@pacificu.edu>
2025-03-01 21:44:19 +0000

    initial code

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,17 @@
 #include <vector> 
 #include <string>
 #include <sstream>
+#include <fstream>
+
+typedef struct TeamDef { 
+	size_t mCount;
+	std::vector<std::string> mLabels;
+} TeamDef;
+
+typedef struct Person {
+	std::string mFname;
+	std::string mLname;
+} Person;
 
 //***************************************************************************
 // Function:    main
@@ -23,7 +34,93 @@
 //***************************************************************************
 int main()
 {
-    std::string cMessage = "hi!";
-    std::cout << ">> " << cMessage << " << " << std::endl;  
-    return EXIT_SUCCESS;
+	std::string cPeopleFilename;
+	std::string cTeamDefFilename;
+	std::string cOutFilename;
+	std::vector <Person> cPeople;
+	
+	std::ifstream cPeopleFile;
+	std::ifstream cTeamDefFile;
+	std::ofstream cOutFile;
+	std::string cLine;
+	TeamDef sTeamDef;
+	Person sPerson;
+
+	std::cout << "What is the filename of the team definition: ";
+	std::cin >> cTeamDefFilename;
+
+	std::cout << "What is the filename of the people: ";
+	std::cin >> cPeopleFilename;
+
+	std::cout << "What is the output filename: ";
+	std::cin >> cOutFilename;
+
+	cTeamDefFile.open (cTeamDefFilename);
+
+	if (cTeamDefFilename.ends_with(".csv")) {
+		while ( getline(cTeamDefFile, cLine) )
+		{
+			std::stringstream cStrStream (cLine);
+			std::string cTmp;
+
+			// get int
+			getline(cStrStream, cTmp, ',');
+			sTeamDef.mCount = std::stoi(cTmp);
+			while (getline(cStrStream, cTmp, ',')) {
+				sTeamDef.mLabels.push_back(cTmp);
+			}
+
+		}
+	} else {
+		// .txt
+		// get int
+		getline(cTeamDefFile, cLine);
+		sTeamDef.mCount = std::stoi(cLine);
+
+		while ( getline(cTeamDefFile, cLine) )
+		{
+			sTeamDef.mLabels.push_back(cLine);
+		}
+	}
+
+	cPeopleFile.open (cPeopleFilename);
+
+	if (cPeopleFilename.ends_with(".csv")) {
+		while ( getline(cPeopleFile, cLine) )
+		{
+			std::stringstream cStrStream (cLine);
+			std::string cTmp;
+
+			while (getline(cStrStream, cTmp, ',')) {
+				sPerson.mFname = cTmp;
+				getline(cStrStream, cTmp, ',');
+				sPerson.mLname = cTmp;
+				cPeople.push_back(sPerson);
+			}
+		}
+	} else {
+		// .txt
+		while ( getline(cPeopleFile, cLine) )
+		{
+			sPerson.mFname = cLine;
+			getline(cPeopleFile, cLine);
+			sPerson.mLname = cLine;
+			cPeople.push_back(sPerson);
+		}
+	}
+
+
+	std::cout << sTeamDef.mCount << std::endl; 
+	for (auto data : sTeamDef.mLabels) {
+		std::cout << data << std::endl;
+	}
+
+	for (auto data : cPeople) {
+		std::cout << data.mFname << " " << data.mLname << std::endl;
+	}
+
+	cPeopleFile.close();
+	cTeamDefFile.close();
+
+  return EXIT_SUCCESS;
 }
